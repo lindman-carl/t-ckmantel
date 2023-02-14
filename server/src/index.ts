df0865e4751525c1c60671b5e69dae1ca5ce7ec3
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-14 19:53:22 +0100

    add: add player to game on connect

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,14 +5,39 @@ import * as dotenv from "dotenv";
 
 // local imports
 import { log } from "./utils.js";
+import {
+  createGame,
+  addPlayerToGame,
+  removePlayerFromGame,
+  logGame,
+} from "./game.js";
 
 dotenv.config();
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// create game
+createGame("0");
 
 io.on("connection", (socket) => {
   log("server", `client connected: ${socket.id}`);
+
+  // add client (player) to game
+  addPlayerToGame("0", socket.id);
+  logGame("0");
+
+  socket.on("disconnect", () => {
+    log("server", `client disconnected: ${socket.id}`);
+
+    // remove client (player) from game
+    removePlayerFromGame("0", socket.id);
+    logGame("0");
+  });
 });
 
 httpServer.listen(process.env.PORT, () => {
