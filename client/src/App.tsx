22c5814ff8279dc7ce0567a937db56d619579819
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-14 21:46:41 +0100

    add: socket.io init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
 const App = () => {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
+  }, []);
   return <div className="App">hej</div>;
 };
 
