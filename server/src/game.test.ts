433aeda4b19b97e0ed22fa870804c4c7b2239be7
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-14 20:46:39 +0100

    add: test addPlayerToGame

--- server/src/game.test.ts
+++ server/src/game.test.ts
@@ -1,6 +1,11 @@
-import { games, createGame } from "./game.js";
+import { games, createGame, addPlayerToGame } from "./game.js";
 
 describe("createGame", () => {
+  beforeAll(() => {
+    // clear games
+    games.clear();
+  });
+
   it("should create a game", () => {
     expect(games.size).toBe(0);
 
@@ -15,5 +20,29 @@ describe("createGame", () => {
     expect(game).toBeDefined();
     expect(game?.id).toBe("test");
     expect(game?.players).toHaveLength(0);
+  });
+});
+
+describe("addPlayerToGame", () => {
+  beforeAll(() => {
+    // clear games
+    games.clear();
+  });
+
+  it("should add a player to a game", () => {
+    // create game
+    createGame("test");
+
+    // add player to game
+    addPlayerToGame("test", "player0");
+
+    // get game
+    const game = games.get("test");
+
+    // expect one player to exist
+    expect(game?.players).toHaveLength(1);
+
+    // expect player to be added
+    expect(game?.players[0]).toBe("player0");
   });
 });
