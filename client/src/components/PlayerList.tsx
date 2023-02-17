aca71aa5b355a82db17fb005f2f637f279fc7cee
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-18 00:24:46 +0100

    add: win count on player list

--- client/src/components/PlayerList.tsx
+++ client/src/components/PlayerList.tsx
@@ -1,4 +1,5 @@
 import DragHandleSvg from "../assets/drag-handle.svg";
+import MedalSvg from "../assets/medal.svg";
 import { Player } from "../types";
 
 type Props = {
@@ -26,18 +27,19 @@ const PlayerList = ({
           {Object.entries(players).map(([id, player]) => (
             <div
               key={id}
-              className="grid h-10 w-full grid-cols-3 rounded-md bg-white px-4 font-medium text-sky-900 shadow"
+              className="grid h-10 w-full grid-cols-5 rounded-md bg-white px-4 font-medium text-sky-900 shadow"
             >
-              <div className="col-span-1 col-start-1 flex h-full items-center justify-start">
+              <div className="col-span-2 col-start-1 flex h-full items-center justify-start">
                 {player.name}
               </div>
-              {player.isHost && (
-                <div className="col-span-1 col-start-2 flex h-full items-center justify-center">
-                  Host
+              {player.wins > 0 && (
+                <div className="col-span-1 col-start-3 flex h-full items-center justify-end">
+                  {player.wins > 1 && <span>{player.wins}x</span>}
+                  <img src={MedalSvg} alt="medal" width="24px" height="24px" />
                 </div>
               )}
               {editable && (
-                <div className="col-span-1 col-start-3 flex h-full items-center justify-end">
+                <div className="col-span-1 col-start-5 flex h-full items-center justify-end">
                   <img
                     src={DragHandleSvg}
                     alt="drag handle"
@@ -48,7 +50,7 @@ const PlayerList = ({
                 </div>
               )}
               {player.inGame && canVote && id !== playerId && (
-                <div className="col-span-1 col-start-3 flex h-full items-center justify-end">
+                <div className="col-span-2 col-start-4 flex h-full items-center justify-end">
                   <button onClick={() => handleVote(id)}>Vote for</button>
                 </div>
               )}
