f6595c88625b6daace525ad5f88ecba476405e0b
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-17 22:26:08 +0100

    add: made menu responsive

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -18,56 +18,60 @@ const Menu = ({ handleJoinGame, handleCreateGame }: MenuProps) => {
   if (menuState === "join") {
     // Join game
     return (
-      <div className="flex w-96 flex-col items-stretch justify-center gap-y-2 rounded-md bg-rose-700 p-4 shadow-inner">
-        <h2 className="text-center text-2xl font-semibold text-white underline decoration-rose-900 decoration-2 underline-offset-2 drop-shadow">
-          Join game
-        </h2>
-        <input
-          type="text"
-          placeholder="Game ID"
-          value={gameId}
-          onChange={(e) => setGameId(e.target.value)}
-          className="w-96 rounded px-4 py-2 text-black shadow-inner"
-        />
-        <input
-          type="text"
-          placeholder="your name"
-          value={playerName}
-          onChange={(e) => setPlayerName(e.target.value)}
-          className="w-96 rounded px-4 py-2 text-black shadow-inner"
-        />
-        <Button
-          onClick={() => handleJoinGame(gameId, playerName)}
-          label="Join Game"
-        />
+      <div className="w-screen px-4 sm:w-96">
+        <div className="flex w-full flex-col items-stretch justify-center gap-y-2 rounded-md bg-rose-700 p-4 shadow-inner">
+          <h2 className="text-center text-2xl font-semibold text-white underline decoration-rose-900 decoration-2 underline-offset-2 drop-shadow">
+            Join game
+          </h2>
+          <input
+            type="text"
+            placeholder="Game ID"
+            value={gameId}
+            onChange={(e) => setGameId(e.target.value)}
+            className="w-96 rounded px-4 py-2 text-black shadow-inner"
+          />
+          <input
+            type="text"
+            placeholder="your name"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+            className="w-96 rounded px-4 py-2 text-black shadow-inner"
+          />
+          <Button
+            onClick={() => handleJoinGame(gameId, playerName)}
+            label="Join Game"
+          />
+        </div>
       </div>
     );
   } else if (menuState === "create") {
     // Create game
     return (
-      <div className="flex w-96 flex-col items-stretch justify-center gap-y-2 rounded-md bg-rose-700 p-4 shadow-inner">
-        <h2 className="text-center text-2xl font-semibold text-white underline decoration-rose-900 decoration-2 underline-offset-2 drop-shadow">
-          Host game
-        </h2>
-        <input
-          type="text"
-          placeholder="game id"
-          value={gameId}
-          onChange={(e) => setGameId(e.target.value)}
-          className="rounded px-4 py-2 text-sky-900 shadow-inner"
-        />
-        <input
-          type="text"
-          placeholder="your name"
-          value={playerName}
-          onChange={(e) => setPlayerName(e.target.value)}
-          className="rounded px-4 py-2 text-sky-900 shadow-inner"
-        />
-        <div className="mx-auto mt-4">
-          <Button
-            onClick={() => handleCreateGame(gameId, playerName)}
-            label="Create Game"
+      <div className="w-screen px-4 sm:w-96">
+        <div className="flex w-full flex-col items-stretch justify-center gap-y-2 rounded-md bg-rose-700 p-4 shadow-inner">
+          <h2 className="text-center text-2xl font-semibold text-white underline decoration-rose-900 decoration-2 underline-offset-2 drop-shadow">
+            Host game
+          </h2>
+          <input
+            type="text"
+            placeholder="game id"
+            value={gameId}
+            onChange={(e) => setGameId(e.target.value)}
+            className="rounded px-4 py-2 text-sky-900 shadow-inner"
           />
+          <input
+            type="text"
+            placeholder="your name"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+            className="rounded px-4 py-2 text-sky-900 shadow-inner"
+          />
+          <div className="mx-auto mt-4">
+            <Button
+              onClick={() => handleCreateGame(gameId, playerName)}
+              label="Create Game"
+            />
+          </div>
         </div>
       </div>
     );
