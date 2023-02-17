0f853dbc58cdf3244568c17348a0396b8ff4beac
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-17 17:39:18 +0100

    add: SignatureFooter

---
+++ client/src/components/SignatureFooter.tsx
@@ -0,0 +1,25 @@
+import OpenInNewSvg from "../assets/open-in-new.svg";
+
+const SignatureFooter = () => {
+  return (
+    <div className="my-4 inline-flex gap-4 text-center text-base font-light text-slate-800">
+      @lindman_dev
+      <a
+        href="https://github.com/yoga-python/tackmantel"
+        target="_blank"
+        rel="noreferrer"
+        className="inline-flex items-center gap-1 font-semibold underline decoration-rose-900 decoration-2 underline-offset-auto "
+      >
+        GitHub
+        <img
+          src={OpenInNewSvg}
+          alt="open in new tab"
+          width="16px"
+          height="16px"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default SignatureFooter;
