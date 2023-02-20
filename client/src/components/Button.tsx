d1add3d2f04a3023e1059b110d19c9c7941a1370
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-20 23:43:38 +0100

    add: min height

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -11,7 +11,7 @@ const Button = ({ onClick, label, alternative = false }: Props) => {
   return (
     <button
       onClick={onClick}
-      className={`flex h-12 min-w-[12rem] max-w-[16rem] items-center justify-center rounded-md px-8 font-bold tracking-wider shadow-lg transition-transform active:scale-95 ${color}`}
+      className={`flex h-12 min-h-[3rem] min-w-[12rem] max-w-[16rem] items-center justify-center rounded-md px-8 font-bold tracking-wider shadow-lg transition-transform active:scale-95 ${color}`}
     >
       {label}
     </button>
