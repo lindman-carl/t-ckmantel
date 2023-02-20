0e8dfc7ba5f529980b31a771c8d2efe32bd106c4
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-20 17:59:57 +0100

    add: alternative design mode

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,13 +1,17 @@
 type Props = {
   onClick: () => void;
   label: string;
+  alternative?: boolean;
 };
 
-const Button = ({ onClick, label }: Props) => {
+const Button = ({ onClick, label, alternative = false }: Props) => {
+  const color = alternative
+    ? "bg-rose-700 text-white"
+    : "bg-slate-800 text-white";
   return (
     <button
       onClick={onClick}
-      className="flex h-12 min-w-[12rem] max-w-[16rem] items-center justify-center rounded-md bg-slate-800 px-8 font-bold tracking-wider text-white shadow-lg transition-transform active:scale-95"
+      className={`flex h-12 min-w-[12rem] max-w-[16rem] items-center justify-center rounded-md px-8 font-bold tracking-wider shadow-lg transition-transform active:scale-95 ${color}`}
     >
       {label}
     </button>
