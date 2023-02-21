382ef0db523cd9339d151042518b3b446ec4e87e
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-21 09:22:49 +0100

    fix: modal being off screen

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Button from "./Button";
 
 type Props = {
@@ -7,15 +8,24 @@ type Props = {
 };
 
 const Modal = ({ heading, message, onClose }: Props) => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
+
+  const handleClose = () => {
+    document.body.style.overflow = "unset";
+    onClose();
+  };
+
   return (
-    <div className="absolute top-0 z-50 flex h-screen w-screen flex-col justify-center bg-black bg-opacity-20 shadow-2xl backdrop-blur-sm">
+    <div className="fixed top-0 z-50 flex h-screen w-screen flex-col justify-center bg-black bg-opacity-20 shadow-2xl backdrop-blur-sm">
       <div className="relative mx-auto flex w-80 flex-col items-center justify-between rounded-md bg-white py-8 text-sky-900 backdrop-blur-sm md:w-96">
         <h2 className="mt-2 mb-4 text-2xl font-bold">{heading}</h2>
         <div className="mb-4 flex w-full items-center justify-center text-center text-lg font-semibold">
           {message}
         </div>
 
-        <Button onClick={onClose} label={"Ok"} />
+        <Button onClick={handleClose} label={"Ok"} />
       </div>
     </div>
   );
