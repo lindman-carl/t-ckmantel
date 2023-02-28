c434dd3f3d41318ecc284e8d13c1cae4d6379a81
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-02-28 23:51:34 +0100

    add: unit tests for gameCreate

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,57 +1,114 @@
-import { games, createGame, addPlayerToGame } from "./game.js";
+import { games, gameCreate, playersInGame } from "./game.js";
 
-describe("createGame", () => {
-  beforeAll(() => {
-    // clear games
+describe("gameCreate", () => {
+  beforeEach(() => {
+    // clear all games and players before each test
     games.clear();
+    playersInGame.clear();
   });
 
-  it("should create a game", () => {
-    expect(games.size).toBe(0);
+  test("should create a game", () => {
+    const gameId = "gameId";
+    const hostId = "hostId";
+    const hostName = "hostName";
 
-    // create game
-    createGame("test", "test-host", "test-player0");
-    const game = games.get("test");
-    if (!game) fail("game not found");
+    const game = gameCreate(gameId, hostId, hostName);
 
-    // expect one game to exist
+    if (game === null) {
+      fail("gameCreate returned null");
+    }
+
+    // expect the returned game object to match the default values
+    expect(game).toEqual({
+      id: gameId,
+      host: hostId,
+      players: {
+        [hostId]: {
+          isUndercover: false,
+          inGame: false,
+          isHost: true,
+          name: hostName,
+          wins: 0,
+          hasVoted: false,
+        },
+      },
+      round: 0,
+      gameOver: false,
+      gameStarted: false,
+      numUndercover: 1,
+      words: {
+        undercover: "undercover",
+        common: "common",
+      },
+      startPlayer: null,
+      votes: [{}],
+      allowVote: false,
+      message: null,
+    });
+
+    // expect the game to be in the games map
+    expect(games.get(gameId)).toEqual(game);
+
+    // expect one game to be in the games map
     expect(games.size).toBe(1);
 
-    // expect default values for the game
+    // expect the host to be added to the players map
+    const hostPlayerObject = game.players[hostId];
+    expect(hostPlayerObject).toBeDefined();
+    expect(hostPlayerObject.name).toBe(hostName);
+    expect(hostPlayerObject.isHost).toBe(true);
+    expect(hostPlayerObject.isUndercover).toBe(false);
+    expect(hostPlayerObject.inGame).toBe(false);
+    expect(hostPlayerObject.wins).toBe(0);
+    expect(hostPlayerObject.hasVoted).toBe(false);
+
+    // expect the host to added to the playersInGame map
+    expect(playersInGame.get(hostId)).toBe(gameId);
+  });
+
+  test("should not create a game if the game id already exists", () => {
+    const gameId = "gameId";
+    const hostId = "hostId";
+    const hostName = "hostName";
+
+    const game = gameCreate(gameId, hostId, hostName);
+    const game2 = gameCreate(gameId, hostId, hostName);
+
+    // expect the first game to be returned
     expect(game).toBeDefined();
-    expect(game?.id).toBe("test");
 
-    // players
-    const playerIds = Object.keys(game.players);
-    expect(playerIds).toHaveLength(0);
+    // expect the duplicate game to be null
+    expect(game2).toBeNull();
 
-    expect(game?.round).toBe(0);
-    expect(game?.numUndercover).toBe(1);
-    expect(game?.words.undercover).toBe("undercover");
-    expect(game?.words.common).toBe("common");
-  });
-});
-
-describe("addPlayerToGame", () => {
-  beforeAll(() => {
-    // clear games
-    games.clear();
+    // expect one game to be in the games map
+    expect(games.size).toBe(1);
   });
 
-  it("should add a player to a game", () => {
-    // create game
-    createGame("test", "test-host", "test-player0");
+  test("should be able to create multiple games", () => {
+    const numGames = 10;
+    const gameIds = Array.from({ length: numGames }, (_, i) => `gameId${i}`);
 
-    // add player to game
-    addPlayerToGame("test", "player-id1", "test-player1");
+    gameIds.forEach((gameId) => {
+      const hostId = `${gameId}hostId`;
+      const hostName = `${gameId}hostName`;
 
-    // get game
-    const game = games.get("test");
-    if (!game) fail("game not found");
+      const game = gameCreate(gameId, hostId, hostName);
 
-    // expect one player to exist
-    const playerIds = Object.keys(game.players);
-    expect(playerIds).toHaveLength(1);
-    expect(playerIds[0]).toBe("player0");
+      if (game === null) {
+        fail("gameCreate returned null");
+      }
+
+      // expect the game to be in the games map
+      expect(games.get(gameId)).toEqual(game);
+
+      // expect the host to added to the playersInGame map
+      expect(playersInGame.get(hostId)).toBe(gameId);
+    });
+
+    // expect the number of games to be in the games map
+    expect(games.size).toBe(numGames);
+
+    // expect the number of players to be in the playersInGame map
+    expect(playersInGame.size).toBe(numGames);
   });
 });
