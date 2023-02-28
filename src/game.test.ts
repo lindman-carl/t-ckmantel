1fc017317ac9cfc9f0959e5e6a4218e171789842
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-03-01 00:25:09 +0100

    add: unit tests for gameAddPlayer

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,4 +1,4 @@
-import { games, gameCreate, playersInGame } from "./game.js";
+import { games, gameCreate, playersInGame, gameAddPlayer } from "./game.js";
 
 describe("gameCreate", () => {
   beforeEach(() => {
@@ -110,5 +110,216 @@ describe("gameCreate", () => {
 
     // expect the number of players to be in the playersInGame map
     expect(playersInGame.size).toBe(numGames);
+  });
+});
+
+describe("gameAddPlayer", () => {
+  beforeEach(() => {
+    // clear all games and players before each test
+    games.clear();
+    playersInGame.clear();
+  });
+
+  test("should add a player to a game", () => {
+    const gameId = "gameId";
+    const hostId = "hostId";
+    const hostName = "hostName";
+    const playerId = "playerId";
+    const playerName = "playerName";
+
+    // create a game
+    gameCreate(gameId, hostId, hostName);
+
+    // add a player to the game
+    const updatedGame = gameAddPlayer(gameId, playerId, playerName);
+
+    if (updatedGame === null) {
+      fail("gameAddPlayer returned null");
+    }
+
+    // get the game from the games map
+    const game = games.get(gameId);
+
+    if (game === undefined) {
+      fail(gameId + "not found in games map");
+    }
+
+    // expect the player to be in the game
+    const playerFromGame = game.players[playerId];
+    expect(playerFromGame).toBeDefined();
+    expect(playerFromGame.name).toBe(playerName);
+    expect(playerFromGame.isHost).toBe(undefined);
+    expect(playerFromGame.isUndercover).toBe(false);
+    expect(playerFromGame.inGame).toBe(false);
+    expect(playerFromGame.wins).toBe(0);
+    expect(playerFromGame.hasVoted).toBe(false);
+
+    // expect the player to be in the correct game
+    const playerInGameId = playersInGame.get(playerId);
+    expect(playerInGameId).toBe(gameId);
+  });
+
+  test("should not add a player to a game if the game does not exist", () => {
+    const gameIdThatDoesntExist = "gameIdThatDoesntExist";
+    const playerId = "playerId";
+    const playerName = "playerName";
+
+    // add a player to the non-existing game
+    const updatedGame = gameAddPlayer(
+      gameIdThatDoesntExist,
+      playerId,
+      playerName
+    );
+
+    // expect the game to be null
+    expect(updatedGame).toBeNull();
+
+    // expect the player to not be added to the playerInGame map
+    expect(playersInGame.size).toBe(0);
+  });
+
+  test("should not add a player to a game if the player is already in a game", () => {
+    const gameId = "gameId";
+    const hostId = "hostId";
+    const hostName = "hostName";
+    const playerId = "playerId";
+    const playerName = "playerName";
+
+    // create a game
+    gameCreate(gameId, hostId, hostName);
+
+    // add a player to the game
+    gameAddPlayer(gameId, playerId, playerName);
+
+    // add the same player to the game again
+    const updatedGame = gameAddPlayer(gameId, playerId, playerName);
+
+    // expect the game to be null
+    expect(updatedGame).toBeNull();
+
+    // expect the player to not be added to the playerInGame map
+    expect(playersInGame.size).toBe(2);
+  });
+
+  test("should be able to add multiple players to a game", () => {
+    const gameId = "gameId";
+    const hostId = "hostId";
+    const hostName = "hostName";
+    const numPlayers = 10;
+    const playerIds = Array.from(
+      { length: numPlayers },
+      (_, i) => `playerId${i}`
+    );
+    const playerNames = Array.from(
+      { length: numPlayers },
+      (_, i) => `playerName${i}`
+    );
+
+    // create a game
+    gameCreate(gameId, hostId, hostName);
+
+    // add players to the game
+    playerIds.forEach((playerId, i) => {
+      const playerName = playerNames[i];
+
+      const updatedGame = gameAddPlayer(gameId, playerId, playerName);
+
+      if (updatedGame === null) {
+        fail("gameAddPlayer returned null");
+      }
+
+      // get the game from the games map
+      const game = games.get(gameId);
+
+      if (game === undefined) {
+        fail(gameId + "not found in games map");
+      }
+
+      // expect the player to be in the game
+      const playerFromGame = game.players[playerId];
+      expect(playerFromGame).toBeDefined();
+      expect(playerFromGame.name).toBe(playerName);
+      expect(playerFromGame.isHost).toBe(undefined);
+      expect(playerFromGame.isUndercover).toBe(false);
+      expect(playerFromGame.inGame).toBe(false);
+      expect(playerFromGame.wins).toBe(0);
+      expect(playerFromGame.hasVoted).toBe(false);
+
+      // expect the player to be in the correct game
+      const playerInGameId = playersInGame.get(playerId);
+      expect(playerInGameId).toBe(gameId);
+    });
+
+    // expect the number of players to be in the playersInGame map
+    expect(playersInGame.size).toBe(numPlayers + 1);
+  });
+
+  test("should be able to add multiple players to multiple games", () => {
+    const numGames = 5;
+    const gameIds = Array.from({ length: numGames }, (_, i) => `gameId${i}`);
+    const hostIds = Array.from({ length: numGames }, (_, i) => `hostId${i}`);
+    const hostNames = Array.from(
+      { length: numGames },
+      (_, i) => `hostName${i}`
+    );
+    const numPlayers = 5;
+    const playerIds = Array.from(
+      { length: numPlayers },
+      (_, i) => `playerId${i}`
+    );
+    const playerNames = Array.from(
+      { length: numPlayers },
+      (_, i) => `playerName${i}`
+    );
+
+    // create games
+    gameIds.forEach((gameId, i) => {
+      const hostId = hostIds[i];
+      const hostName = hostNames[i];
+
+      gameCreate(gameId, hostId, hostName);
+    });
+
+    // add players to the games
+    gameIds.forEach((gameId) => {
+      playerIds.forEach((playerId, i) => {
+        const playerName = gameId + playerNames[i];
+        const playerIdWithGameId = gameId + playerId;
+
+        const updatedGame = gameAddPlayer(
+          gameId,
+          playerIdWithGameId,
+          playerName
+        );
+
+        if (updatedGame === null) {
+          fail("gameAddPlayer returned null");
+        }
+
+        // get the game from the games map
+        const game = games.get(gameId);
+
+        if (game === undefined) {
+          fail(gameId + "not found in games map");
+        }
+
+        // expect the player to be in the game
+        const playerFromGame = game.players[playerIdWithGameId];
+        expect(playerFromGame).toBeDefined();
+        expect(playerFromGame.name).toBe(playerName);
+        expect(playerFromGame.isHost).toBe(undefined);
+        expect(playerFromGame.isUndercover).toBe(false);
+        expect(playerFromGame.inGame).toBe(false);
+        expect(playerFromGame.wins).toBe(0);
+        expect(playerFromGame.hasVoted).toBe(false);
+
+        // expect the player to be in the correct game
+        const playerInGameId = playersInGame.get(playerIdWithGameId);
+        expect(playerInGameId).toBe(gameId);
+      });
+    });
+
+    // expect the number of players to be in the playersInGame map
+    expect(playersInGame.size).toBe(numGames * numPlayers + numGames);
   });
 });
