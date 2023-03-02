b0079eeda1214fa0292c922f5cea884fbed68e17
yoga-python <73431769+yoga-python@users.noreply.github.com>
2023-03-02 18:38:03 +0100

    add: header to player list

--- client/src/components/PlayerList.tsx
+++ client/src/components/PlayerList.tsx
@@ -1,14 +1,11 @@
-import MedalSvg from "../assets/medal.svg";
 import { Player } from "../types";
 
 type Props = {
   players: { [id: string]: Player };
-  canKick: boolean;
   canVote: boolean;
   playerId: string;
   hasStarted: boolean;
   handleVote: (voteForId: string) => void;
-  handleKick: (playerId: string) => void;
 };
 
 const getDesign = (inGame: boolean, hasStarted: boolean, hasVoted: boolean) => {
@@ -29,41 +26,44 @@ const getDesign = (inGame: boolean, hasStarted: boolean, hasVoted: boolean) => {
 
 const PlayerList = ({
   players,
-  canKick,
   canVote,
   playerId,
   handleVote,
-  handleKick,
   hasStarted,
 }: Props) => {
   return (
     <div className="flex flex-col items-center justify-center gap-y-2">
+      <div className="grid h-8 w-full grid-cols-8 rounded-md bg-rose-800 px-4 font-medium text-white shadow-inner">
+        <div className="col-span-4 col-start-1 flex h-full items-center justify-start">
+          Player
+        </div>
+        <div className="col-span-2 col-start-7 flex h-full items-center justify-end">
+          Score
+        </div>
+      </div>
       {Object.entries(players).map(([id, player]) => (
         <div
           key={id}
-          className={`grid h-10 w-full grid-cols-5 px-4 font-medium ${getDesign(
+          className={`grid h-10 w-full grid-cols-8 grid-rows-1 px-4 font-medium ${getDesign(
             player.inGame,
             hasStarted,
             player.hasVoted
           )}`}
         >
-          <div className="col-span-2 col-start-1 flex h-full items-center justify-start">
+          <div className="col-span-4 col-start-1 row-start-1 flex h-full items-center justify-start">
             {player.name}
           </div>
-          {player.wins > 0 && (
-            <div className="col-span-1 col-start-3 flex h-full items-center justify-end">
-              {player.wins > 1 && <span>{player.wins}x</span>}
-              <img src={MedalSvg} alt="medal" width="24px" height="24px" />
-            </div>
-          )}
-          {canKick && id !== playerId && (
-            <div className="col-span-1 col-start-5 flex h-full items-center justify-end">
-              <button onClick={() => handleKick(id)}>Kick</button>
-            </div>
-          )}
+          <div className="col-span-1 col-start-8 row-start-1 flex h-full items-center justify-end">
+            {player.score}
+          </div>
           {player.inGame && canVote && id !== playerId && (
-            <div className="col-span-2 col-start-4 flex h-full items-center justify-end">
-              <button onClick={() => handleVote(id)}>Vote for</button>
+            <div className="col-span-3 col-start-5 row-start-1 flex h-full items-center justify-center">
+              <button
+                onClick={() => handleVote(id)}
+                className="py-0.1 rounded-lg border-2 border-rose-800 bg-rose-700 bg-checkered-pattern px-4 text-white shadow transition-all active:scale-95 active:bg-rose-800 active:bg-none active:text-rose-900 active:shadow-inner"
+              >
+                Vote
+              </button>
             </div>
           )}
         </div>
